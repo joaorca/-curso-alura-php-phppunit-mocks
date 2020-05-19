8dc5d2989e781ee516dce6fa773e51777a798c7f
João R C Almeida <joaorca@unisuam.edu.br>
2020-05-18 22:13:09 -0300

    Criando um dublê de teste

--- tests/Service/EncerradorTest.php
+++ tests/Service/EncerradorTest.php
@@ -7,6 +7,41 @@ use Alura\Leilao\Model\Leilao;
 use Alura\Leilao\Service\Encerrador;
 use PHPUnit\Framework\TestCase;
 
+class LeilaoDaoMock extends LeilaoDao
+{
+    /** @var Leilao[] */
+    private array $leiloes = [];
+
+    public function salva(Leilao $leilao): void
+    {
+        $this->leiloes[] = $leilao;
+    }
+
+    public function recuperarFinalizados(): array
+    {
+        return array_filter(
+            $this->leiloes,
+            function (Leilao $leilao) {
+                return $leilao->estaFinalizado();
+            }
+        );
+    }
+
+    public function recuperarNaoFinalizados(): array
+    {
+        return array_filter(
+            $this->leiloes,
+            function (Leilao $leilao) {
+                return !$leilao->estaFinalizado();
+            }
+        );
+    }
+
+    public function atualiza(Leilao $leilao)
+    {
+    }
+}
+
 class EncerradorTest extends TestCase
 {
     public function testLeiloesComMaisDeUmaSemanaDevemSerEncerrados()
@@ -21,11 +56,11 @@ class EncerradorTest extends TestCase
             new \DateTimeImmutable('10 days ago')
         );
 
-        $leilaoDao = new LeilaoDao();
+        $leilaoDao = new LeilaoDaoMock();
         $leilaoDao->salva($fiat147);
         $leilaoDao->salva($variant);
 
-        $encerrador = new Encerrador();
+        $encerrador = new Encerrador($leilaoDao);
         $encerrador->encerra();
 
         $leiloes = $leilaoDao->recuperarFinalizados();
