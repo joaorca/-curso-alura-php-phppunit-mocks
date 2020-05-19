a1294e89d5e257c2a961a537047e6d5c516099bb
João R C Almeida <joaorca@unisuam.edu.br>
2020-05-18 22:26:45 -0300

    Perguntando ao dublê

--- src/Dao/Leilao.php
+++ src/Dao/Leilao.php
@@ -2,16 +2,15 @@
 
 namespace Alura\Leilao\Dao;
 
-use Alura\Leilao\Infra\ConnectionCreator;
 use Alura\Leilao\Model\Leilao as ModelLeilao;
 
 class Leilao
 {
-    private $con;
+    private \PDO $con;
 
-    public function __construct()
+    public function __construct(\PDO $con)
     {
-        $this->con = ConnectionCreator::getConnection();
+        $this->con = $con;
     }
 
     public function salva(ModelLeilao $leilao): void
